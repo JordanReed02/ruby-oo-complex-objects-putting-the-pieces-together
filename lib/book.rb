92292410b9ceaf616bae8bbcece883029650fc17
Joran <jordanalireed@icloud.com>
2020-11-19 02:20:09 -0500

    commit tree cd34ba1e86a1d578b2ee227feb20c1a02de6ed0e

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,16 @@
+# book.#!/usr/bin/env ruby -wKU
 
+class Book
+  attr_accessor :author, :page_count, :genre, :turn_page
+  attr_reader :title, :turn_page
 
+  def initialize(title)
+    @title = title
+    end
+
+def turn_page
+    def turn_page
+      puts "Flippng the page...wow, you read fast!"
+    end
+
+end
